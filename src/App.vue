5809e01a963d1e42b4c81df8cf58c52b60378972
Andres Zuleta <afleon386@misena.edu.co>
2024-02-02 11:47:57 -0500

    repo creado

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+let fruta = ['fresa', 'mango', 'kiwi']
+
+
+
+
+</script>
+
+<template>
+
+   <ul v-for="(frutas, key) in fruta" :key="key">
+    <li >{{ frutas }}</li>
+   </ul>
+
+</template>
+
+
+<style scoped>
+
+</style>
